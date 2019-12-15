101275952e0b1ceddb55efd273a13d2b16bd579c
Mateus Sauer Govêa <mateus.sauer@gmail.com>
2019-12-15 16:38:34 -0300

    Android Test

---
+++ mobile/app/src/main/java/br/com/alura/technews/ui/viewmodel/ListaNoticiasViewModel.kt
@@ -0,0 +1,16 @@
+package br.com.alura.technews.ui.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class ListaNoticiasViewModel : ViewModel() {
+
+    init {
+        Log.i("viewmodel", "criando viewmodel")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("viewmodel", "destruindo viewmodel")
+    }
+}
