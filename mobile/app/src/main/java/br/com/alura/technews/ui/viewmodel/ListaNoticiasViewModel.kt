874435a679647f06d51b2cea9285a2cbc5ce74e8
Mateus Sauer Govêa <mateus.sauer@gmail.com>
2019-12-21 10:52:30 -0300

    aula viewmodel

--- mobile/app/src/main/java/br/com/alura/technews/ui/viewmodel/ListaNoticiasViewModel.kt
+++ mobile/app/src/main/java/br/com/alura/technews/ui/viewmodel/ListaNoticiasViewModel.kt
@@ -2,15 +2,15 @@ package br.com.alura.technews.ui.viewmodel
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import br.com.alura.technews.model.Noticia
+import br.com.alura.technews.repository.NoticiaRepository
 
-class ListaNoticiasViewModel : ViewModel() {
+class ListaNoticiasViewModel (private val repository: NoticiaRepository ) : ViewModel() {
 
-    init {
-        Log.i("viewmodel", "criando viewmodel")
+
+    fun buscaTodos(quandoSucesso: (List<Noticia>) -> Unit, quandoFalha: (String?) -> Unit) {
+        repository.buscaTodos(quandoSucesso, quandoFalha)
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        Log.i("viewmodel", "destruindo viewmodel")
-    }
+
 }
